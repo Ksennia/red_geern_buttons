3993e8ff7b520ee706edff6bdab18c398f9f4dc6
ksenia <kseniya.binkovskaya@gmail.com>
2021-08-10 15:05:35 +0300

    add app to Git

---
+++ red_geern_buttons/src/main/java/com/example/red_geern_buttons/RedGeernButtonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.red_geern_buttons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedGeernButtonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedGeernButtonsApplication.class, args);
+	}
+
+}
---
+++ red_geern_buttons/src/test/java/com/example/red_geern_buttons/RedGeernButtonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.red_geern_buttons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedGeernButtonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
